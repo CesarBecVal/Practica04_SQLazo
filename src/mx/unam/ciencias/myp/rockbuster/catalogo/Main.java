7081079b7a82d099b2d30cdb6a154d41985c3bcf
CesarBecval <cesarbecerravalencia@gmail.com>
2025-10-02 13:20:40 -0600

    feat: main implementado

---
+++ src/mx/unam/ciencias/myp/rockbuster/catalogo/Main.java
@@ -0,0 +1,8 @@
+package mx.unam.ciencias.myp.rockbuster.catalogo;
+
+import mx.unam.ciencias.myp.rockbuster.catalogo.Menu;
+
+public static void main (String args[]) {
+    Menu menu = new Menu();
+    menu.run();
+}
