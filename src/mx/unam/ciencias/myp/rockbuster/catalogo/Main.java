6f9231572c773e2edc148c72c2942dafb2123e45
CesarBecval <cesarbecerravalencia@gmail.com>
2025-10-04 14:29:25 -0600

    fix: Documentacion agregada al main.

--- src/mx/unam/ciencias/myp/rockbuster/catalogo/Main.java
+++ src/mx/unam/ciencias/myp/rockbuster/catalogo/Main.java
@@ -1,9 +1,35 @@
 package mx.unam.ciencias.myp.rockbuster.catalogo;
+
 import java.util.Scanner;
 import mx.unam.ciencias.myp.rockbuster.catalogo.service.Catalog;
 
-
+/**
+ * The entry point of the Rockbuster Catalog application.
+ * <p>
+ * This class initializes the main components of the system, including
+ * the {@link Catalog} and the {@link Menu}, and starts the program's main loop.
+ * </p>
+ * 
+ * <p>
+ * Usage:
+ * <pre>
+ * java mx.unam.ciencias.myp.rockbuster.catalogo.Main
+ * </pre>
+ * </p>
+ * 
+ * @author  César
+ */
 public class Main {
+
+    /**
+     * Main method that starts the Rockbuster application.
+     * <p>
+     * It creates a {@link Catalog} instance, initializes a {@link Scanner} to handle
+     * user input, and launches the main {@link Menu} loop.
+     * </p>
+     *
+     * @param args command-line arguments (not used)
+     */    
     public static void main (String args[]) {
         Catalog catalogo = new Catalog();
         Scanner sc = new Scanner(System.in);
